9a582270cf37096088ec1991e59faed042e273a9
tsarenkoivn <162137146+tsarenkoivn@users.noreply.github.com>
2024-10-16 22:15:11 +0200

    finished task1

---
+++ Proj1/TestProject1/UnitTests1.cs
@@ -0,0 +1,61 @@
+﻿using Tut2_s20123;
+
+namespace Tut2_s20123_Tests
+{
+    public class Tests
+    {
+        private readonly IShape sphere = new Sphere(5);
+        private readonly IShape cylinder = new Cylinder(5, 12);
+        private readonly IShape rectangle = new Rectangle(6, 8);
+        private readonly IShape cube = new Cube(4);
+
+        [Test]
+        public void TestSphereCalculateArea()
+        {
+            Assert.That(sphere.CalculateArea(), Is.EqualTo(314.159).Within(0.001));
+        }
+
+        [Test]
+        public void TestSphereCalculateVolume()
+        {
+            Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
+        }
+
+        [Test]
+        public void TestCubeCalculateArea()
+        {
+            Assert.That(cube.CalculateArea(), Is.EqualTo(96.000).Within(0.001));
+        }
+
+        [Test]
+        public void TestCubeCalculateVolume()
+        {
+            Assert.That(cube.CalculateVolume(), Is.EqualTo(64.000).Within(0.001));
+        }
+
+        [Test]
+        public void TestCylinderCalculateArea()
+        {
+            Assert.That(cylinder.CalculateArea(), Is.EqualTo(534.070).Within(0.001));
+        }
+
+        [Test]
+        public void TestCylinderCalculateVolume()
+        {
+            Assert.That(cylinder.CalculateVolume(), Is.EqualTo(942.477).Within(0.001));
+        }
+
+        [Test]
+        public void TestRectangleCalculateArea()
+        {
+            Assert.That(rectangle.CalculateArea(), Is.EqualTo(48.000).Within(0.001));
+        }
+
+        [Test]
+        public void TestRectangleCalculateVolume()
+        {
+            Assert.That(rectangle.CalculateVolume(), Is.EqualTo(0.000).Within(0.001));
+        }
+
+    }
+}
